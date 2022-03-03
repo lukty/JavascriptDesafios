dc1f4603175c8e727c48c0a0a9b6d65829f95121
Lucas Bringas <lucas.bringas71@gmail.com>
2022-03-02 22:30:59 -0300

    DesafioComplementario

--- desafioComplementario.js
+++ desafioComplementario.js
@@ -1,7 +1,7 @@
 
 // Arrays
 
-class pantalon {
+class camperas {
     constructor(id, nombre, color, precio) {
       this.id = id;
       this.nombre = nombre;
@@ -10,29 +10,29 @@ class pantalon {
     }
   }
   
-  const pantalon = [];
-  pantalon.push(new pantalon(1, "Bulgaria", "roja", 2500));
-  pantalon.push(new pantalon(2, "Roma", "roja", 1500));
-  pantalon.push(new pantalon(3, "madrid", "roja", 2000));
-  pantalon.push(new pantalon(4, "Bulgaria", "azul", 2000));
-  pantalon.push(new pantalon(5, "Roma", "azul", 2500));
-  pantalon.push(new pantalon(6, "madrid", "azul", 1500));
-  pantalon.push(new pantalon(7, "Bulgaria", "blanca", 2000));
-  pantalon.push(new pantalon(8, "Roma", "blanca", 2500));
-  pantalon.push(new pantalon(9, "madrid", "blanca", 1500));
+  const camperas = [];
+  camperas.push(new camperas(1, "Bulgaria", "roja", 2500));
+  camperas.push(new camperas(2, "Roma", "roja", 1500));
+  camperas.push(new camperas(3, "madrid", "roja", 2000));
+  camperas.push(new camperas(4, "Bulgaria", "azul", 2000));
+  camperas.push(new camperas(5, "Roma", "azul", 2500));
+  camperas.push(new camperas(6, "madrid", "azul", 1500));
+  camperas.push(new camperas(7, "Bulgaria", "blanca", 2000));
+  camperas.push(new camperas(8, "Roma", "blanca", 2500));
+  camperas.push(new camperas(9, "madrid", "blanca", 1500));
   
-  console.log(pantalon); /* ver el nuevo array */
+  console.log(camperas); /* ver el nuevo array */
   
   const listaTipo = [];
   const listaPrecio = [];
   const listaColor = [];
   
   let pregunta = prompt(
-    "Que quieres consultar hoy:\n 1-Tipos de pantalon 2-Precios 3-Colores"
+    "Que quieres consultar hoy:\n 1-Tipos de camperas 2-Precios 3-Colores"
   );
   if (pregunta == 1) {
     console.log("hola respuesta TIPO");
-    for (const i of pantalon) {
+    for (const i of camperas) {
       listaTipo.push(i.nombre);
       console.log(listaTipo);
       console.log(typeof listaTipo);
@@ -43,7 +43,7 @@ class pantalon {
     alert(result.join(" - "));
   } else if (pregunta == 2) {
     console.log("hola respuesta PRECIO");
-    for (const i of pantalon) {
+    for (const i of camperas) {
       listaPrecio.push(i.precio);
       console.log(listaPrecio);
       console.log(typeof listaPrecio);
@@ -54,7 +54,7 @@ class pantalon {
     alert(result.join(" - "));
   } else {
     console.log("ultima respuesta");
-    for (const i of pantalon) {
+    for (const i of camperas) {
       listaColor.push(i.color);
       console.log(listaColor);
       console.log(typeof listaColor);
